02590fb3706da2646dc4874a21aba4c54b76bc15
joshuaodermatt <joshi.odermatt@gmail.com>
2021-04-29 10:25:12 +0200

    fixed js validation

--- public/js/validation.js
+++ public/js/validation.js
@@ -2,18 +2,24 @@ const form = document.getElementById('form');
 
 form.addEventListener('submit', function(event) {
 
+    console.log('js is here')
 
+    const lastnameElement = document.getElementById('lastname');
+    const firstnameElement = document.getElementById('firstname');
+    const emailElement = document.getElementById('email');
+    const phoneElement = document.getElementById('phone');
+    const concertElement = document.getElementById('concert');
 
-    const lastname = document.getElementById('lastname').value;
-    const firstname = document.getElementById('firstname').value;
-    const email = document.getElementById('email').value;
-    const phone = document.getElementById('phone').value;
-    const concert = document.getElementById('concert').value;
+    const lastname = lastnameElement.value;
+    const firstname = firstnameElement.value;
+    const email = emailElement.value;
+    const phone = phoneElement.value;
+    const concert = concertElement.value;
 
-    lastname.style.borderColor = '#dbdbdb';
-    firstname.style.borderColor = '#dbdbdb';
-    email.style.borderColor = '#dbdbdb';
-    phone.style.borderColor = '#dbdbdb';
+    lastnameElement.style.borderColor = '#dbdbdb';
+    firstnameElement.style.borderColor = '#dbdbdb';
+    emailElement.style.borderColor = '#dbdbdb';
+    phoneElement.style.borderColor = '#dbdbdb';
 
     email.trim();
     phone.trim()
@@ -24,26 +30,26 @@ form.addEventListener('submit', function(event) {
 
     if(lastname === '') {
         errors.push('Bitte geben Sie einen Nachnamen an');
-        lastname.style.borderColor = red;
+        lastnameElement.style.borderColor = red;
     }
 
     if(firstname === '') {
         errors.push('Bitte geben Sie einen Vornamen an');
-        firstname.style.borderColor = red;
+        firstnameElement.style.borderColor = red;
     }
 
 
     if (email=== '') {
         errors.push('Bitte geben Sie eine E-mail an');
-        email.style.borderColor = red;
+        emailElement.style.borderColor = red;
     }else if (!email.match('/(.+)@(.+){2,}\\.(.+){2,}')) {
         errors.push('Bitte geben Sie eine Valide Email ein.');
-        email.style.borderColor = red;
+        emailElement.style.borderColor = red;
     }
 
-    if(!phone.value.match('/^(?:(?:|0{1,2}|\\+{0,2})41(?:|\\(0\\))|0)([1-9]\\d)(\\d{3})(\\d{2})(\\d{2})$/')) {
+    if(!phone.match('/^(?:(?:|0{1,2}|\\+{0,2})41(?:|\\(0\\))|0)([1-9]\\d)(\\d{3})(\\d{2})(\\d{2})$/')) {
         errors.push('Bitte geben Sie eine Valide Telefonnummer ein.');
-        phone.style.borderColor = red;
+        phoneElement.style.borderColor = red;
     }
 
 
@@ -52,11 +58,10 @@ form.addEventListener('submit', function(event) {
     }
 
     if (errors.length > 0) {
-        const errorList = document.getElementById('error-list');
 
         event.preventDefault();
 
-        console.log('prevented default')
+        const errorList = document.getElementById('error-list');
 
         errorList.innerHTML = "";
 
